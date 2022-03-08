a9650eb5bc067584b0665a38e0cd3f2106834419
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-03-08 11:23:16 +0700

    update:create server Api

--- app.js
+++ app.js
@@ -1,18 +1,57 @@
 const http = require('http');
+const express = require('express');
 
-const server = http.createServer((req, res) => {
-    console.log('url', req.url);
-    if(req.url === "/") {
-        console.log("Home Page");
-    }else if(req.url === "/api"){
-        console.log("API");
+const app = express();
+app.use(express.json());
+
+// middleware
+const check = (req, res, next) => {
+    const status = true;
+    if(status) {
+        console.log("Hello");
+        next();
     }else{
-        console.log("404");
+        console.log("Fail");
     }
-    console.log("complete");
-});
+}
+app.use(check)
+// app.use((req, res,next) => {
+//     console.log('Step 2');
+//     next();
+// })
+// app.get('/',(req, res,next) => {
+//     console.log('Home');
+//     next();
+// })
+app.get('/api/products',check ,(req, res,) => {
+    const products = [
+        {id: 1, name: 'Product A'},
+        {id: 2, name: 'Product B'},
+        {id: 3, name: 'Product C'}
+    ];
+    res.json(products);
+})
+// const server = http.createServer((req, res) => {
+//     console.log('url', req.url);
+//     if(req.url === "/") {
+//         res.setHeader('Content-Type', 'text/html');
+//         res.write("<html><body><h1>Home page</h1></body></html>");
+//         res.end();
+//         console.log("Home Page");
+//     }else if(req.url === "/api/products"){
+//         const products=[
+//             {id:1 , name:"product A"},
+//             {id:2 , name:"product B"},
+//         ];
+//         res.end(JSON.stringify(products));
+//         console.log("API");
+//     }else{
+//         console.log("404");
+//     }
+//     console.log("complete");
+// });
 
 const PORT = 3001;
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server is running port",PORT);
 });
