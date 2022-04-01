dcddbfddfe2b68db9492ca1de4012282adb9af44
Lê Thanh Bình <lethanhbinh26062002@gmail.com>
2022-04-02 03:00:17 +0700

    4/2

--- models/middlewares/checkAuth.js
+++ models/middlewares/checkAuth.js
@@ -1,31 +1,27 @@
 import expressJWT from "express-jwt";
 export const checkAuth = (req, res, next) => {
-    const status = true;
-    if(status){
-        next();
-    } else {
-        console.log("Lỗi truy cập");
-    };
+
 };
-export const requireSignin =  expressJWT({
+export const requireSignin = expressJWT({
     algorithms: ["HS256"], // Key "HS256"
     secret: "bin26",
-    requestProperty:"auth" // Gán thông tin vào req.auth
+    requestProperty: "auth" // Gán thông tin vào req.auth
 })
-export const isAuth = (req, res,next) => {
-    const status = req.profiles._id = req.auth._id; // So sánh id giữa clien vs server
-    // console.log("req.profiles",req.profiles);
-    // console.log("req.auth",req.auth);
-    if(!status) {
+export const isAuth = (req, res, next) =>{
+    console.log ('req.profile', req.profiles._id);
+    console.log ('req.auth', req.auth._id);
+
+    const status = req.profiles._id = req.auth._id
+    if(!status){
         res.status(400).json({
-            message: "Bạn không có quyền truy cập"
+        message: "Không có quyền truy cập"
         })
     }
     next();
 }
 export const checkAdmin = (req, res, next) => {
     const checkRole = req.auth.role = 1;
-    if(!checkRole) {
+    if (!checkRole) {
         res.status(400).json({
             message: "Bạn không có quyền admin"
         })
--- router/product.js
+++ router/product.js
@@ -7,8 +7,8 @@ const router = Router();
 router.get('/products' ,list);
 router.get('/products/:id' ,real);
 router.post('/product/:userId',requireSignin,isAuth,checkAdmin ,create);
-router.delete('/products/:id',requireSignin,isAuth,checkAdmin ,remove);
-router.put('/products/:id/edit',requireSignin,isAuth,checkAdmin ,update);
+router.delete('/products/:userId/:id',requireSignin,isAuth,checkAdmin ,remove);
+router.put('/products/:userId/:id/edit',requireSignin,isAuth,checkAdmin ,update);
 
 router.param("userId",userById) // Lấy userId từ URL rồi check user có tồn tại  => next()
 
